7ebe54a8ee90b779554fbd78736f4890f912445c
rks6004 <rks6004@psu.edu>
2024-10-21 19:47:21 -0400

    ongoing fixes/debugging statements

--- mdadm.c
+++ mdadm.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include "mdadm.h"
 #include "jbod.h"
+#include <tester.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -11,51 +12,54 @@ int mdadm_mounted = FALSE;
 
 int mdadm_mount(void) {
   // Complete your code here
-  printf("\ncurrent JBOD status before mounting: %d\n", jbod_error);
+  //printf("\ncurrent JBOD status before mounting: %d\n", jbod_error);
   if (mdadm_mounted) {
     //only need to fill out "op" field since all other fields ignored for mounting
-    printf("current JBOD status after array already mounted: %d\n", jbod_error);
+    //printf("current JBOD status after array already mounted: %d\n", jbod_error);
     return -1;
   }
   int jbod_op_return =  jbod_operation((uint32_t)(JBOD_MOUNT << RESERVED_WIDTH), NULL);
-  printf("jbod_operation return value during jbod_mount func: %d\n", jbod_op_return);
+  //printf("jbod_operation return value during jbod_mount func: %d\n", jbod_op_return);
   if (jbod_op_return == -1) {
-    printf("current JBOD status after failed mounting: %d\n", jbod_error);
+    // printf("current JBOD status after failed mounting: %d\n", jbod_error);
     return -1;  
   }
-  printf("current JBOD status after successful mounting: %d\n", jbod_error);
+  // printf("current JBOD status after successful mounting: %d\n", jbod_error);
   mdadm_mounted = TRUE;
   return 1;
 }
 
 int mdadm_unmount(void) {
   //Complete your code here
-  printf("\ncurrent JBOD status before unmounting: %d\n", jbod_error);
+  // printf("\ncurrent JBOD status before unmounting: %d\n", jbod_error);
   if (!mdadm_mounted) {
     //only need to fill out "op" field since all other fields ignored for mounting
-    printf("current JBOD status after array already unmounted: %d\n", jbod_error);
+    // printf("current JBOD status after array already unmounted: %d\n", jbod_error);
     return -1;
   }
   int jbod_op_return =  jbod_operation((uint32_t)(JBOD_UNMOUNT << RESERVED_WIDTH), NULL);
-  printf("jbod_operation return value during mdam_unmount func: %d\n", jbod_op_return);
+  // printf("jbod_operation return value during mdam_unmount func: %d\n", jbod_op_return);
   if (jbod_op_return == -1) {
-    printf("current JBOD status after failed unmounting: %d\n", jbod_error);
+    // printf("current JBOD status after failed unmounting: %d\n", jbod_error);
     return -1;  
   }
-  printf("current JBOD status after successful unmounting: %d\n", jbod_error);
+  // printf("current JBOD status after successful unmounting: %d\n", jbod_error);
   mdadm_mounted = FALSE;
   return 1;
 }
 
 //disk locator to use in opcode population
 int disk_locator(uint32_t addr) {
-  return (addr / JBOD_DISK_SIZE);
+  int disk_calculated = (addr)/JBOD_DISK_SIZE;
+  return disk_calculated;
 }
 
 //block locator to use in opcode population
 int block_locator(uint32_t addr) {  
-  uint32_t disk_addr = addr - (disk_locator(addr) * JBOD_BLOCK_SIZE);
-  return disk_addr / JBOD_BLOCK_SIZE; 
+  //get BYTE-BASED address of addr
+  int disk_location = disk_locator(addr);
+  uint32_t byte_based_addr = addr - (disk_location * JBOD_DISK_SIZE);
+  return byte_based_addr / JBOD_BLOCK_SIZE; 
 }
 
 int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
@@ -70,10 +74,7 @@ int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
     return BAD_ARGS;
   }
   if (read_len == 0) {
-    if (read_buf == NULL) {
-      return read_len;
-    }
-    return ZERO_LENGTH_READ;
+    return 0;
   }
   if (!mdadm_mounted) {
     return ARRAY_UNMOUNTED;
@@ -84,29 +85,31 @@ int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
   //+ read_len bytes is less than max block addr.
   //memcpy works direct here- can just read read_len bytes from start_addr into read_buf loc.
   if ((start_addr % JBOD_BLOCK_SIZE) + read_len < JBOD_BLOCK_SIZE) {
+    printf("\nwe are NOT reading beyond a single block.\n");
     //opcode for seeking disk, referencing disk_locator helpful func
     uint32_t seek_disk_code = (disk_locator(start_addr) 
       << (sizeof(seek_disk_code)*8 - DISK_ID_WIDTH)) 
       + (JBOD_SEEK_TO_DISK << RESERVED_WIDTH); 
-    jbod_operation(seek_disk_code, NULL);
+    if (jbod_operation(seek_disk_code, NULL) == -1) {return -4;}
     //opcode for seeking disk, referencing block_locator helpful func
     uint32_t seek_block_code = (block_locator(start_addr) 
       << (sizeof(seek_block_code)*8 - DISK_ID_WIDTH - BLOCK_ID_WIDTH)) 
       + (JBOD_SEEK_TO_DISK << RESERVED_WIDTH); 
-    jbod_operation(seek_block_code, NULL);
+    if (jbod_operation(seek_block_code, NULL) == -4) {return -4;}
     uint32_t read_code = JBOD_READ_BLOCK << RESERVED_WIDTH;
     //would this work?
     //type asserts 1 byte, yet size declaration states 256 bytes
     uint8_t temp_buffer[256];
-    jbod_operation(read_code, temp_buffer);
+    if (jbod_operation(read_code, temp_buffer) == -4) {return -4;}
     //copying memory from where start_addr lies IN BLOCK
     //for read_len bits
     memcpy(read_buf, temp_buffer + (start_addr % JBOD_BLOCK_SIZE), read_len);
   }
   //reading beyond a single block
   else {
+    printf("\nwe are reading beyond a single block.");
     uint32_t end_addr = start_addr + read_len;
-    printf("start address: %d, end address calculated as: %d\n", start_addr, end_addr);
+    printf("\nstart address: %u, end address calculated as: %u\n", start_addr, end_addr);
     int start_disk = disk_locator(start_addr);
     int end_disk = disk_locator(end_addr);
     printf("starting at disk: %d and ending at disk: %d\n", start_disk, end_disk);
@@ -126,13 +129,15 @@ int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
       else {
         end_block = JBOD_NUM_BLOCKS_PER_DISK - 1;
       }
-      uint32_t seek_block_code = (start_block << (RESERVED_WIDTH + CMD_ID_WIDTH))
-        + (JBOD_SEEK_TO_BLOCK << RESERVED_WIDTH);
+      printf("\nFor disk: %d, reading from start block: %d to end block: %d\n", d, start_block, end_block);
+      uint32_t seek_block_code = 
+          (start_block << (sizeof(seek_disk_code)*8 - DISK_ID_WIDTH))
+          + (JBOD_SEEK_TO_BLOCK << RESERVED_WIDTH);
       jbod_operation(seek_block_code, NULL);
       //increment will be used for TRACKING purposes- JBOD_READ increments block value itself
       //will still have to reference b in order to jump "back" to new disk when necessary
       //since JBOD_READ will likely throw errors if reading beyond disk
-      for (int b = block_locator(addr_tracker); b <= end_block; b++) {
+      for (int b = start_block; b <= end_block; b++) {
         uint32_t read_code = JBOD_READ_BLOCK << RESERVED_WIDTH;
         uint8_t temp_buffer[256];
         jbod_operation(read_code, temp_buffer);
@@ -143,17 +148,22 @@ int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
         uint8_t temp_bytes;
         //if within last block, read number of EXCESS bytes to read
         if (b == end_block) {
-          temp_bytes = end_addr % JBOD_BLOCK_SIZE;
+          temp_bytes = (end_addr % JBOD_BLOCK_SIZE) - addr_tracker_byte;
         }
         //otherwise, read till end of block
         else {
-          temp_bytes = JBOD_BLOCK_SIZE - addr_tracker_byte - 1;
+          temp_bytes = JBOD_BLOCK_SIZE - addr_tracker_byte;
         }
+        printf("\nFor block %d, reading %u bytes\n", b, temp_bytes);
         //operation sees temp_bytes being copied from addr_tracker's BLOCK BASED value
         //to read_buf at its "latest" location
         memcpy(read_buf + bytes_read, temp_buffer + addr_tracker_byte, temp_bytes);
+        // printf("Addr_tracker was at: %u", addr_tracker);
+        printf("Total bytes read before block operation: %d\n", bytes_read);
+        printf("Bytes read during block operation: %d\n", temp_bytes);
         addr_tracker += temp_bytes;
         bytes_read += temp_bytes;
+        printf("Total bytes read after block operation: %d\n", bytes_read);
       }
     }
   }
